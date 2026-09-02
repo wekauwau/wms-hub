934f20ba1a962e24e5a4a61ba4b1c17bda0c0c06
wekauwau <wekauwau@gmail.com>
2026-09-02 16:40:46 +0700

    feat: env file for configuring api/web port

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -6,10 +6,8 @@ server.get('/health', async () => {
   return { status: 'ok' }
 })
 
-const port = Number(process.env['PORT']) || 3000
-
 try {
-  await server.listen({ port, host: '0.0.0.0' })
+  await server.listen({ port: Number(process.env['PORT']) || 3000, host: '0.0.0.0' })
 } catch (err) {
   server.log.error(err)
   process.exit(1)
